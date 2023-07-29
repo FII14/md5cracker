41f533d2351a372034c953aaa1232b8505ca47e6
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-29 21:57:49 +0700

    Update md5cracker.php

--- md5cracker.php
+++ md5cracker.php
@@ -13,20 +13,32 @@ function crackMd5Hash($hash, $wordlistPath) {
 }
 
 // Parse command-line arguments
-$shortOpts = "h:w:";
+$shortOpts = "h:w:h";
 $longOpts = array(
     "hash:",
-    "wordlist:"
+    "wordlist:",
+    "help"
 );
 
 $options = getopt($shortOpts, $longOpts);
+
+if (isset($options['help'])) {
+    echo "Usage: php your_script.php [OPTIONS]\n";
+    echo "Crack an MD5 hash using a wordlist.\n";
+    echo "\n";
+    echo "Options:\n";
+    echo "  -h, --hash         MD5 hash to crack (required)\n";
+    echo "  -w, --wordlist     Path to the wordlist file (required)\n";
+    echo "  --help             Show this help message and exit\n";
+    exit(0);
+}
 
 if (isset($options['h'])) {
     $hashToCrack = $options['h'];
 } elseif (isset($options['hash'])) {
     $hashToCrack = $options['hash'];
 } else {
-    die("Error: Missing required argument -h/--hash for MD5 hash.");
+    die("Error: Missing required argument -h/--hash for MD5 hash. Use --help for usage information.\n");
 }
 
 if (isset($options['w'])) {
@@ -34,7 +46,7 @@ if (isset($options['w'])) {
 } elseif (isset($options['wordlist'])) {
     $wordlistPath = $options['wordlist'];
 } else {
-    die("Error: Missing required argument -w/--wordlist for wordlist path.");
+    die("Error: Missing required argument -w/--wordlist for wordlist path. Use --help for usage information.\n");
 }
 
 $result = crackMd5Hash($hashToCrack, $wordlistPath);
