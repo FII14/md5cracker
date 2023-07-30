b8aa6c93d81dbe0f3b19104036a1550ed1d9b27b
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-30 08:40:42 +0700

    Update download_rockyou.py

--- download_rockyou.py
+++ download_rockyou.py
@@ -1,13 +1,5 @@
-import requests
+import os
 
 link = "https://gitlab.com/kalilinux/packages/wordlists/-/raw/kali/master/rockyou.txt.gz?ref_type=heads"
-nama_file = "rockyou.txt.gz"
 
-try:
-  response = requests.get(link)
-  response.raise_for_status()
-  with open(nama_file, 'wb') as file:
-    file.write(response.content)
-    print("Download completed!")
-except requests.exceptions.RequestException as e:
-  print(f"Error occurred: {e}")
+os.systme(f"wget {link}")
